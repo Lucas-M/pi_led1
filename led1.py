ba7c2b44ebccbc653bcf181856fd125e95c7e31a
Luke <lucas.macd@gmail.com>
2016-02-15 21:02:46 -0500

    Updated print statements to python3..added parentheses to print statements

--- led1.py
+++ led1.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python/
 import os                       # what is this?
 import time			# why import this?
 import RPi.GPIO as GPIO		# Import lib to interface with GPIO pin on rpi2
@@ -15,16 +16,16 @@ count = 0
 
 os.system('clear')
 
-print "Which LED you want to blink?"
-print "!: Red?"
-print "2: Green?"
-print "3: Yellow?"
-print "4: All?"
+print ("Which LED you want to blink?")
+print ("1: Red?")
+print ("2: Green?")
+print ("3: Yellow?")
+print ("4: All?")
 led_choice = input("Make your choice: ")
 
 if led_choice == 1:
     os.system('clear')
-    print "You choose the red LED"
+    print ("You choose the red LED")
     count = input("How many times you want it to blink?: ")
     while count > 0:
         GPIO.output(7,GPIO.HIGH)
@@ -35,7 +36,7 @@ if led_choice == 1:
 
 if led_choice == 2:
     os.system('clear')
-    print "You choose the green LED"
+    print ("You choose the green LED")
     count = input("How many times you want it to blink?: ")
     while count > 0:
         GPIO.output(11,GPIO.HIGH)
@@ -46,7 +47,7 @@ if led_choice == 2:
 
 if led_choice == 3:
     os.system('clear')
-    print "You choose the yellow LED"
+    print ("You choose the yellow LED")
     count = input("How many times you want it to blink?: ")
     while count > 0:
         GPIO.output(13,GPIO.HIGH)
@@ -57,7 +58,7 @@ if led_choice == 3:
 
 if led_choice == 4:
     os.system('clear')
-    print "You choose all LEDs"
+    print ("You choose all LEDs")
     count = input("How many times you want them all to blink?: ")
     while count > 0:
         GPIO.output(7,GPIO.HIGH)
